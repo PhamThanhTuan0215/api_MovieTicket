907db1bb5ccfc6b566669112852b64776b6015c9
PhamThanhTuan0215 <thanhtuancr1234@gmail.com>
2023-12-20 14:51:42 +0700

    coin

--- models/account.js
+++ models/account.js
@@ -5,7 +5,8 @@ const AccountSchema = new Schema({
     username: { type: String, unique: true },
     email: { type: String, unique: true },
     phone: { type: String, unique: true },
-    password: String
+    password: String,
+    coin: {type: Number, default: 0}
 })
 
 module.exports = mongoose.model('Account', AccountSchema)
